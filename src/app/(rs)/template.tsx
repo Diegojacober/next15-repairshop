b599725b71748a6646ad014f6b72ee938591175d
Diego <diegoalencar.jacober@gmail.com>
2024-12-27 19:54:05 -0300

    creating initial ui

---
+++ src/app/(rs)/template.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default async function RSTemplate({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div className="animate-appear">{children}</div>;
+}
---
+++ src/app/(rs)/tickets/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Tickets() {
+  return <h2>Tickets Page</h2>;
+}
+
+export default Tickets;
